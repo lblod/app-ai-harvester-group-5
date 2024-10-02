328384095160621316be32cc78153c28193a73aa
Michaël Dierick <michael.dierick@redpencil.io>
2024-10-02 13:14:53 +0200

    add delta config for ai service + disable blocking services

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -240,70 +240,70 @@ export default [
       ignoreFromSelf: true,
     },
   },
-  {
-    match: {
-      predicate: {
-        type: "uri",
-        value: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
-      },
-      object: {
-        type: "uri",
-        value: "http://vocab.deri.ie/cogs#ScheduledJob",
-      },
-    },
-    callback: {
-      method: "POST",
-      url: "http://scheduled-job-controller/delta",
-    },
-    options: {
-      resourceFormat: "v0.0.1",
-      //Allow more time (10s) for the frontend to have saved everything. If the
-      //scheduled-job exists before its tasks and authentication configuration,
-      //then the scheduled job service tries to query that data before it is
-      //written to the triplestore, failing to encrypt the secrets for the
-      //scheduled task.
-      gracePeriod: 10000,
-      ignoreFromSelf: true,
-    },
-  },
-  {
-    match: {
-      predicate: {
-        type: "uri",
-        value: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
-      },
-      object: {
-        type: "uri",
-        value: "http://redpencil.data.gift/vocabularies/deltas/Error",
-      },
-    },
-    callback: {
-      url: "http://delta-producer-report-generator/delta",
-      method: "POST",
-    },
-    options: {
-      resourceFormat: "v0.0.1",
-      gracePeriod: 1000,
-      ignoreFromSelf: true,
-    },
-  },
-  {
-    match: {
-      predicate: {
-        type: "uri",
-        value: "http://www.w3.org/ns/adms#status",
-      },
-    },
-    callback: {
-      url: "http://delta-producer-report-generator/delta",
-      method: "POST",
-    },
-    options: {
-      resourceFormat: "v0.0.1",
-      gracePeriod: 1000,
-      ignoreFromSelf: true,
-    },
-  },
+  // {
+  //   match: {
+  //     predicate: {
+  //       type: "uri",
+  //       value: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+  //     },
+  //     object: {
+  //       type: "uri",
+  //       value: "http://vocab.deri.ie/cogs#ScheduledJob",
+  //     },
+  //   },
+  //   callback: {
+  //     method: "POST",
+  //     url: "http://scheduled-job-controller/delta",
+  //   },
+  //   options: {
+  //     resourceFormat: "v0.0.1",
+  //     //Allow more time (10s) for the frontend to have saved everything. If the
+  //     //scheduled-job exists before its tasks and authentication configuration,
+  //     //then the scheduled job service tries to query that data before it is
+  //     //written to the triplestore, failing to encrypt the secrets for the
+  //     //scheduled task.
+  //     gracePeriod: 10000,
+  //     ignoreFromSelf: true,
+  //   },
+  // },
+  // {
+  //   match: {
+  //     predicate: {
+  //       type: "uri",
+  //       value: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+  //     },
+  //     object: {
+  //       type: "uri",
+  //       value: "http://redpencil.data.gift/vocabularies/deltas/Error",
+  //     },
+  //   },
+  //   callback: {
+  //     url: "http://delta-producer-report-generator/delta",
+  //     method: "POST",
+  //   },
+  //   options: {
+  //     resourceFormat: "v0.0.1",
+  //     gracePeriod: 1000,
+  //     ignoreFromSelf: true,
+  //   },
+  // },
+  // {
+  //   match: {
+  //     predicate: {
+  //       type: "uri",
+  //       value: "http://www.w3.org/ns/adms#status",
+  //     },
+  //   },
+  //   callback: {
+  //     url: "http://delta-producer-report-generator/delta",
+  //     method: "POST",
+  //   },
+  //   options: {
+  //     resourceFormat: "v0.0.1",
+  //     gracePeriod: 1000,
+  //     ignoreFromSelf: true,
+  //   },
+  // },
   {
     match: {
       graph: {
@@ -351,6 +351,27 @@ export default [
     },
     callback: {
       url: "http://delta-producer-dump-file-publisher/delta",
+      method: "POST",
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+    },
+  },
+  {
+    match: {
+      predicate: {
+        type: "uri",
+        value: "http://www.w3.org/ns/adms#status",
+      },
+      object: {
+        type: "uri",
+        value: "http://redpencil.data.gift/id/concept/JobStatus/scheduled",
+      },
+    },
+    callback: {
+      url: "http://ai-service/delta",
       method: "POST",
     },
     options: {
